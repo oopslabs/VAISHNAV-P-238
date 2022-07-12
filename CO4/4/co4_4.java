a5cc98793d1071d9302f7065ed2e3ddec3dec2f0
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2022-07-12 16:23:45 +0530

    Add files via upload

---
+++ CO4/4/co4_4.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class co4_4
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the limit");
+		int n=s.nextInt();
+		int t=0,avg=0,m=n;
+		System.out.println("Enter the numbers ");
+		for(int i=0;i<n;i++)
+		{
+			try
+			{
+				t=s.nextInt();
+				if(t<0)
+				{
+					n++;
+					throw new neg_exec("Negative number entered");
+				}
+				else
+				{
+					avg+=t;
+				}
+			}
+			catch(neg_exec e)
+			{
+				System.out.println(e.getMessage());
+			}
+		}
+		System.out.println("Average of n positive numbers: "+(avg/m));
+		
+	}
+}
+
+class neg_exec extends Exception
+{
+	neg_exec(String msg)
+	{
+		super(msg);
+	}
+}
