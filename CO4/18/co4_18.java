1f5d4be5fa664585e9b65a3d7e7123061cca617f
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2022-07-12 22:49:57 +0530

    Add files via upload

---
+++ CO4/18/co4_18.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class co4_18
+{
+	public static void main(String[] args)
+	{
+		Map<Integer,String> hm=new HashMap<>();
+		hm.put(1,"One");
+		hm.put(2, "Two");
+	     	hm.put(15, "Fifteen");
+	      hm.put(7, "Seven");
+	      hm.put(3, "Three");
+	     	hm.put(9, "Nine");	
+	     	System.out.println("Hashmap: "+hm);
+	   	
+	     	Map<Integer,String> tm=new TreeMap<>();
+	     	tm.putAll(hm);
+	     	System.out.println("\nTreeMap from HashMap: "+tm);
+	     		
+		
+	}
+}
