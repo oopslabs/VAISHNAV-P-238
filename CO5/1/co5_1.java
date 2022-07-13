e5860af12eba14167ca818c0d99f9892eb51b315
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2022-07-13 21:03:02 +0530

    Add files via upload

---
+++ CO5/1/co5_1.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+public class co5_1 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawLine(20,20,50,20);
+		
+		g.drawOval(200,200,100,100);
+		
+		g.drawRect(100,100,100,100);
+	}
+}
+/*<applet code = "co5_1.class" width=300 height=250></applet>*/
