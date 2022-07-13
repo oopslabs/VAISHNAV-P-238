c27e32e4a2456a9a19fdfdbc4733ff545ef9d2dc
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2022-07-13 22:39:38 +0530

    Add files via upload

---
+++ CO6/co6_1.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class co6_1
+{
+	public static void main(String[] args) 
+	{
+		File file = new File("C:/MCA/OOP_LAB/COS/CO1");
+		String[] list = file.list();
+		for(String str : list)
+		{
+			System.out.println(str);
+		}
+		System.out.println("\nSEARCHING FOR FILE NAMES STARTING WITH 's':\n");
+		FilenameFilter filter = new FilenameFilter() 
+		{
+			public boolean accept(File dir, String fname) 
+			{
+				return fname.startsWith("s");
+			}
+		}; 
+		String[] search =file.list(filter);
+		if(search.length==0)
+		{
+			System.out.println("File does not exist.");
+		}
+		else 
+		{
+			for(int i=0; i<search.length;i++) 
+			{
+				String fn = search[i];
+				System.out.println(fn);
+			}
+		}
+	}
+}
